a2a87c8bcb1b1046b1ae60b9a3d41fa31be22655
Konami33 <yasinarafat9889@gmail.com>
2024-09-20 00:45:52 +0600

    Deploy Frontend and backend image using Github action

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,49 @@
-require('dotenv').config();
+// require('dotenv').config();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const db = require('./models');
+// const userRoutes = require('./routes/user');
+// const homeRoutes = require('./routes/home'); 
+// const cors = require('cors');
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// // using cors
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use('/', homeRoutes);
+// app.use('/users', userRoutes);
+
+// // start server
+// const startServer = async () => {
+//     try {
+//         await db.sequelize.sync();
+//         console.log("Connected to database successfully.");
+//         app.listen(PORT, () => {
+//             console.log(`Server is running on port ${PORT}`);
+//         });
+//     } catch (err) {
+//         console.error("Unable to sync database:", err);
+//     }
+// };
+
+// // calling the function
+// startServer();
 const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./models');
-const userRoutes = require('./routes/user');
-const homeRoutes = require('./routes/home'); 
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 4000;
 
-// using cors
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/', homeRoutes);
-app.use('/users', userRoutes);
+app.use(cors()); // Enable CORS for all routes
 
-// start server
-const startServer = async () => {
-    try {
-        await db.sequelize.sync();
-        console.log("Connected to database successfully.");
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (err) {
-        console.error("Unable to sync database:", err);
-    }
-};
+app.get("/", (req, res) => {
+    const message = "Hello world from the backend! 🚀";
+    console.log(message);
+    res.json({ message });
+});
 
-// calling the function
-startServer();
+app.listen(PORT, () => {
+    console.log(`Server Running at port ${PORT}`);
+});
